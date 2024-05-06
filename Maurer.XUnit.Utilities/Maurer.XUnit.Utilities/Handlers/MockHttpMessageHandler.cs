43ea6a0856aac648b03a000ff0a0802322a48724
john-maurer <jmau0438@gmail.com>
2024-05-05 22:02:19 -0500

    initial commit

---
+++ Maurer.XUnit.Utilities/Maurer.XUnit.Utilities/Handlers/MockHttpMessageHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maurer.XUnit.Utilities.Handlers
+{
+    public class MockHttpMessageHandler : HttpMessageHandler
+    {
+        private readonly Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> _handler;
+
+        public MockHttpMessageHandler(Func<HttpRequestMessage, CancellationToken, Task<HttpResponseMessage>> handler) =>
+            _handler = handler;
+
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken) =>
+            _handler(request, cancellationToken);
+    }
+}
